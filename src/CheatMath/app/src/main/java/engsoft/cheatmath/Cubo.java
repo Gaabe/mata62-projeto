b74b7b02a009647a65750fe7eaa961be61c0589c
Gaabe <gabriel.c94@gmail.com>
2016-10-25 00:02:03 -0300

    finalizacao da interface basica do aplicativo, atualizacao dos diagramas de sequencia e de classes, e das telas

--- src/CheatMath/app/src/main/java/engsoft/cheatmath/Cubo.java
+++ src/CheatMath/app/src/main/java/engsoft/cheatmath/Cubo.java
@@ -6,8 +6,17 @@ public class Cubo{
     public void Cubo(int aresta){
         this.setAresta(aresta);
     }
+
     public int calculate_volume(){
         return this.getAresta() * this.getAresta() * this.getAresta();
+    }
+
+    public String show_result(){
+        String result = "Volume -> V    Aresta -> l \n" +
+                "V = a³ \n" +
+                "V = 3³ \n" +
+                "V = " + Integer.toString(calculate_volume());
+        return result;
     }
 
     public int getAresta() {
--- src/CheatMath/app/src/main/java/engsoft/cheatmath/DisplayVolCubo.java
+++ src/CheatMath/app/src/main/java/engsoft/cheatmath/DisplayVolCubo.java
@@ -1,10 +1,14 @@
 package engsoft.cheatmath;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class DisplayVolCubo extends AppCompatActivity {
 
@@ -16,8 +20,6 @@ public class DisplayVolCubo extends AppCompatActivity {
 
     public void calculate(View view){
         EditText editText = (EditText) findViewById(R.id.editAresta);
-        System.out.println("integer:");
-        System.out.println(editText.getText().toString());
         int aresta = Integer.parseInt(editText.getText().toString());
         Cubo cubo = new Cubo();
         cubo.setAresta(aresta);
@@ -25,6 +27,16 @@ public class DisplayVolCubo extends AppCompatActivity {
         String result = Integer.toString(resultado);
         TextView textView = (TextView) findViewById(R.id.textResultado);
         textView.setText(result);
+        TextView solucao = (TextView) findViewById(R.id.textSolucao);
+        solucao.setText(cubo.show_result());
 
+    }
+
+    public void favorite(View view){
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.favorites), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.favorites), "volume cubo");
+        editor.commit();
+        Toast.makeText(getApplicationContext(), "Formula adicionada aos favoritos", Toast.LENGTH_SHORT).show();
     }
 }
